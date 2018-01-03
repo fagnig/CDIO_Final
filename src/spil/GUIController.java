e54b7259450d75324a1043d51220cccd3d4d41c3
fagnig <fagnig@autistklassen.dk>
2018-01-03 13:07:29 +0100

    first pass dice & guicontroller

--- src/spil/GUIController.java
+++ src/spil/GUIController.java
@@ -1,12 +1,70 @@
 package spil;
 
+import java.awt.Color;
+
+import gui_fields.*;
+import gui_main.*;
+
 public class GUIController {
 	private static final GUIController guiController = new GUIController();
-	
+	private GUI_Field[] fieldsGUI;
 	private GUIController(){}
+	private GUI gui;
 	
 	public GUIController getInstance()
 	{
 		return guiController;	
+	}
+	
+	public void initFields()
+	{
+		//ModelController mC = modelController.getInstance();
+		//Field[] fields = mC.getBoard().getFields();
+		//for(int i=0; i>=fields.length(), i++){
+		//	if fields[i].getType() == 0{
+		//		fieldsGUI[i] = new GUI_Start(fields[i].getName(), fields[i].getSubText(), fields[i].getDescription(), fields[i].getPrimaryColor(), fields[i].getSecondaryColor());
+		//	}
+		//}
+		
+		//gui = new GUI(fieldsGUI);
+	}
+	
+	public void showMessage(String message) 
+	{
+		gui.showMessage(message);
+	}
+	
+	public void showChanceCard(String message) 
+	{
+		gui.displayChanceCard(message);
+	}
+	
+	public void updateDice(int[] t) 
+	{
+		gui.setDice(t[0], t[1]);
+	}
+	
+	public void getYesNo(String message) 
+	{
+		//if (gui.getUserButtonPressed(message, Language.yes(), Language.no())==Language.yes())
+		//{
+		//	return true
+		//} else {
+		//	return false
+		//}
+	}
+	
+	public int getInteger(String message, int[] limits) 
+	{
+		return gui.getUserInteger(message, limits[0], limits[1]);
+	}
+	public int getInteger(String message) 
+	{
+		return gui.getUserInteger(message);
+	}
+	
+	public String getString(String message) 
+	{
+		return gui.getUserString(message);
 	}
 }
