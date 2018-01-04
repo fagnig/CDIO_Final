0d2bd00720844ec0fe01d0ee065ac314d4e7a4bd
fagnig <fagnig@autistklassen.dk>
2018-01-04 14:01:24 +0100

    wallet class

--- src/spil/Wallet.java
+++ src/spil/Wallet.java
@@ -2,11 +2,19 @@ package spil;
 
 public class Wallet {
     private int[] fields = new int[40];
-    private int jailCard;
+    private int jailCard= 0;
     private int balance;
 
     public int getTotalValue(){
         //fill in
         return 1;
+    }
+
+    public void addMoney(int amount){
+        balance = balance + amount;
+    }
+
+    public Wallet(int newBalance){
+        balance = newBalance;
     }
 }
