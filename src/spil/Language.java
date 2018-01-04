7ecc216ee7edfc392793f5aa8ff99a2556cc0a26
fagnig <fagnig@autistklassen.dk>
2018-01-04 20:15:14 +0100

    formatted description added to language

--- src/spil/Language.java
+++ src/spil/Language.java
@@ -8,4 +8,14 @@ public class Language {
 	public static String no() {
 		return "Nej";
 	}
+
+	public static String getFormattedDescription(int[] dat){
+		String tempStr = "Leje: " + Integer.toString(dat[0]) +
+				"\n 1 Hus: " + Integer.toString(dat[1]) +
+				"\n 2 Huse: " + Integer.toString(dat[2]) +
+				"\n 3 Huse: " + Integer.toString(dat[3]) +
+				"\n 4 Huse: " + Integer.toString(dat[4]) +
+				"\n Hotel: " + Integer.toString(dat[5]);
+		return tempStr;
+	}
 }
