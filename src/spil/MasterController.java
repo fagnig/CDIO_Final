d0fdb58826ae4f50bcad04faf3144844cbf74547
fagnig <fagnig@autistklassen.dk>
2018-01-04 13:08:12 +0100

    modelmeme

--- src/spil/MasterController.java
+++ src/spil/MasterController.java
@@ -2,7 +2,12 @@ package spil;
 
 public class MasterController {
 
+    public static int PLAYER_STARTBALANCE = 30000;
+    public static int GAME_PLAYERS_MAX = 6;
+    public static int GAME_PLAYERS_MIN = 2;
+
 	public static void main(String[] args) {
+
 		GUIController guiC = GUIController.getInstance();
 		//guiC.initFields();
 		//guiC.showMessage("hej");
