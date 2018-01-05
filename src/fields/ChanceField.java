22f164fcaad4c9a02414683cc6a2bfe964c093e0
trolleren <trolleren@trollerens>
2018-01-05 22:11:54 +0100

    lol im retard

---
+++ src/fields/ChanceField.java
@@ -0,0 +1,24 @@
+package fields;
+
+import java.awt.Color;
+
+import model.Player;
+
+public class ChanceField extends NeutralField {
+
+	public ChanceField(String newName, Color primary, Color secondary) {
+		super(newName, primary, secondary);
+		// TODO Auto-generated constructor stub
+		type = 1;
+	}
+
+	@Override
+	public void landOnField(Player landingPlayer) {
+		// TODO Auto-generated method stub
+		
+	}
+    public int getType() {
+    	return type;
+    }
+
+}
