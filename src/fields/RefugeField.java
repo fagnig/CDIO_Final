59309fd3c3c1cccae4a417c8be028545efa7f35d
Nicki1994 <nicki.christiansen94@gmail.com>
2018-01-08 12:04:42 +0100

    refuge og board opdatering

---
+++ src/fields/RefugeField.java
@@ -0,0 +1,22 @@
+package fields;
+
+import java.awt.Color;
+
+import model.Player;
+
+public class RefugeField extends NeutralField {
+
+	public RefugeField(String newName, Color primary, Color secondary) {
+		super(newName, primary, secondary);
+		// TODO Auto-generated constructor stub
+		type = 7;
+	}
+	public int getType() {
+		return type;
+	}
+	@Override
+	public void landOnField(Player landingPlayer) {
+		// TODO Auto-generated method stub
+		
+	}
+}
