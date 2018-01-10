7faa6e7dd3052964e323d70aa8db5d7444ad3a0c
trolleren <trolleren@trollerens>
2018-01-10 04:43:35 +0100

    DiceController opdateret

--- src/controllers/MasterController.java
+++ src/controllers/MasterController.java
@@ -12,16 +12,17 @@ public class MasterController {
 
 	public static void main(String[] args)  {
 		int currentturn = 0;
+		DiceController cup = new DiceController();
 		GUIController guiC = GUIController.getInstance();
 		ModelController modC = ModelController.getInstance();
 		guiC.initFields();
 		Player[] players = modC.makePlayers(guiC.makePlayers());
 		while (true) {
 			guiC.getOk(Language.roll());
-			players[currentturn].move(modC.roll());
+			players[currentturn].move(cup.roll());
 			players[currentturn].receiveMoney(500);
-			guiC.updateGUI(players,modC.getFaces());
-			guiC.upgrade(players[currentturn].getLocation());
+			guiC.updateGUI(players,cup.getFaces());
+
 			currentturn = (currentturn+1) %(players.length);
 			
 			
@@ -32,7 +33,7 @@ public class MasterController {
 		
 		
 		
-		
+		//			guiC.upgrade(players[currentturn].getLocation());
 		
 	}
 
--- src/model/Dice.java
+++ src/model/Dice.java
@@ -3,33 +3,18 @@ package model;
 import java.util.Random;
 
 public class Dice {
-	private int faceValue, t1, t2;
+	private int faceValue;
 	
 	Random dice = new Random(); 
 	
 	
-	public void roll() {
-		t1 = dice.nextInt(6)+1;
-		t2 = dice.nextInt(6)+1;
-		faceValue = t1+t2 ;
+	public int roll() {
+		faceValue = dice.nextInt(6)+1;
+		return faceValue;
+
 	}
-	
 	public int getFaceValue() {
 		return faceValue;
 	}
-	
-	public int[] getFaces(){
-		int[] faces = new int[2];
-		faces[0] = t1;
-		faces[1] = t2;
-		return faces;
-	}
-	
-	public boolean getDouble() {
-		if (t1 == t2) {
-			return true; 
-		}else{
-			return false;
-		}
-	}
+
 }
